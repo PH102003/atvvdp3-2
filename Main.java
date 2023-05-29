811cef69ff9485776668628906ac4cee6a8d4f6e
PH102003 <pedroh.rj2003@hotmail.com>
2023-05-28 22:53:56 -0300

    .

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Cliente c = new Cliente();
-        c.extrato();
+        Cliente c = new Cliente(null, 0, null, null, 0, null);
+        c.atualizarHistorico(null);
     }
 }
